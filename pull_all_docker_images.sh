bbdbb62ff32ee4d6127cd8477f91b868eeed2fbd
pfcui <cuipengfei2008@gmail.com>
2016-12-16 16:08:12 +0800

    pull all docker images

---
+++ pull_all_docker_images.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+images=(
+"requisition"
+"postgres"
+"rsyslog"
+"requisition-refui"
+"referencedata"
+"auth"
+"notification"
+"dev"
+"fulfillment"
+"nginx"
+"openlmis-nginx"
+"dev-ui"
+"openlmis-example-extensions"
+)
+
+for image in ${images[@]}
+do
+    echo "going to pull docker image: $image"
+    docker pull "openlmis/$image" &
+done
