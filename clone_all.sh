d862f488550afd28bdff51a0edf2dda651b765ef
pfcui <cuipengfei2008@gmail.com>
2016-12-16 11:54:12 +0800

    define all the repo names included in ol3

---
+++ clone_all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+. ./repos.sh
+
+for repoName in ${repoNames[@]}
+do
+    echo $repoName
+done
---
+++ repos.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+services=(
+"openlmis-requisition" "openlmis-referencedata"
+"openlmis-fulfillment" "openlmis-auth"
+"openlmis-notification" )
+
+infrastructures=(
+"openlmis-rsyslog" "docker-dev"
+"postgres" "nginx-proxy"
+"openlmis-nginx")
+
+tests=("openlmis-contract-tests")
+
+devops=("openlmis-deployment" "openlmis-blue")
+
+examples=(
+"openlmis-example" "openlmis-template-service"
+"openlmis-example-extensions")
+
+ui=("openlmis-requisition-refUI"  "dev-ui")
+
+config=("openlmis-config")
+
+repoNames=("${services[@]}" "${infrastructures[@]}" "${tests[@]}" "${devops[@]}" "${examples[@]}" "${ui[@]}" "${config[@]}")
